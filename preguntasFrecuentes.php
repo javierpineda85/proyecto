6983a1405110f67f25b4c766bb519735815f9405
Pablo Garcia Alvarez <pabloga90@gmail.com>
2019-06-17 20:07:18 -0300

    Creacion Preguntas Frecuentes
    Cree la seccion de Preguntas Frecuentes, no esta la informacion, es solo la estructura. Formato Responsive.

---
+++ preguntasFrecuentes.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8">
+   		<link rel="stylesheet" href="css/stylesForms.css"> 
+    	<link rel="stylesheet" href="css/preFrecuentes.css" >
+    	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css" >
+    	<link rel="shortcut icon" href="img/faviconasa.ico">
+	
+	<title> Preguntas Frecuentes .. </title>
+</head>
+<body class="preFrecuentes">
+	<br>
+	<div class="container" id="preFrecuentes">
+	
+	<h1>Preguntas Frecuentes</h1><br>
+		<section class="preguntasFrecuentes">
+		<h2>¿Pregunta Nº 1?</h2> <br>
+			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+		</section>
+	<br><br><br>
+		<section class="preguntasFrecuentes">
+		<h2>¿Pregunta Nº 2?</h2> <br>
+			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+		</section>
+	<br><br><br>
+		<section class="preguntasFrecuentes">
+		<h2>¿Pregunta Nº 3?</h2> <br>
+			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+		</section>
+	<br><br><br>
+		<section class="preguntasFrecuentes">
+		<h2>¿Pregunta Nº 4?</h2> <br>
+			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+		</section>
+	<br><br><br>
+		<section class="preguntasFrecuentes">
+		<h2>¿Pregunta Nº 5?</h2> <br>
+			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+		</section>
+	</div>
+
+</body>
+</html>
