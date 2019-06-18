b956226e44d7116065f816e54366c150725e8e13
nicobende <niko_bendele@hotmail.com>
2019-06-17 21:15:40 -0300

    Creacion footer. php y css

---
+++ footer.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <title>Hello, world!</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/stilofooter.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato%7CMontserrat" rel="stylesheet">
+    <script src="https://unpkg.com/ionicons@4.5.5/dist/ionicons.js"></script>
+</head>
+
+  <body class="container">
+  <section class="containerfooter">
+    <nav class="footer">
+      <div class="logoasa">
+        <img class="logoasa" src="img/asalogo.png" alt="">
+        <h3 class="logoasa">MENDOZA</h3>
+        <br>
+        <br>
+      </div>
+      <br>
+      <div class="secciones">
+        <h3>SECCIONES</h3>
+        <section>
+          <ul>
+  					<li><a href="#">INICIO</a></li>
+  					<li><a href="#">PROYECTOS</a></li>
+  					<li><a href="#">SERVICIOS</a></li>
+  					<li><a href="#">NOSOTROS</a></li>
+  					<li><a href="#">CONTACTENOS</a></li>
+  				</ul>
+        </section>
+      </div>
+
+      <div class="contactos">
+        <h3>CONTACTOS</h3>
+        <section>
+  				<p>DUARDO Arquitectos SL / P. de la Catellana 79, Madrid 28046.</p>
+        </section>
+      </div>
+
+      <div class="redes">
+        <h3>REDES</h3>
+        <ion-icon name="logo-facebook"></ion-icon>
+        <ion-icon name="logo-googleplus"></ion-icon>
+        <ion-icon name="logo-twitter"></ion-icon>
+        <ion-icon name="logo-instagram"></ion-icon>
+        <ion-icon name="logo-skype"></ion-icon>
+      </div>
+      <br>
+      <div class="piedepagina">
+        <p>Diseñado y programado por
+        <b>bigtable.com - 2019°</b></p>
+      </div>
+    </nav>
+  </section>
+  </body>
+</html>
