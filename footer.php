08a8b1cd9fa26fb35f270459cbfe65403a4b5bbe
nicobende <niko_bendele@hotmail.com>
2019-06-20 20:42:52 -0300

    Footer y menu
    Menu y footer responsive en proceso.
    Se agrego otro archivo del ASA

--- footer.php
+++ footer.php
@@ -3,59 +3,78 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <title>Hello, world!</title>
+    <title>FOOTER</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/stilofooter.css">
     <link href="https://fonts.googleapis.com/css?family=Lato%7CMontserrat" rel="stylesheet">
     <script src="https://unpkg.com/ionicons@4.5.5/dist/ionicons.js"></script>
+    <style type="text/css" media="screen">
+    A:link {color: white; text-decoration: none }
+    A:hover {color: white;  text-decoration: none }
+    A:visited {color: white;  text-decoration: none }
+    </style>
 </head>
+  <body>
+    <header>
 
-  <body class="container">
-  <section class="containerfooter">
-    <nav class="footer">
-      <div class="logoasa">
-        <img class="logoasa" src="img/asalogo.png" alt="">
-        <h3 class="logoasa">MENDOZA</h3>
-        <br>
-        <br>
-      </div>
-      <br>
-      <div class="secciones">
-        <h3>SECCIONES</h3>
-        <section>
-          <ul>
-  					<li><a href="#">INICIO</a></li>
-  					<li><a href="#">PROYECTOS</a></li>
-  					<li><a href="#">SERVICIOS</a></li>
-  					<li><a href="#">NOSOTROS</a></li>
-  					<li><a href="#">CONTACTENOS</a></li>
-  				</ul>
-        </section>
-      </div>
-
-      <div class="contactos">
-        <h3>CONTACTOS</h3>
-        <section>
-  				<p>DUARDO Arquitectos SL / P. de la Catellana 79, Madrid 28046.</p>
-        </section>
-      </div>
+    </header>
+   <footer class="containerfooter">
+     <section>
+       <nav class="footer">
+        <div class="logoasa">
+           <img class="" src="img/asalogo2.png" alt="">
+           <h3>MENDOZA</h3>
+        </div>
+        <div class="secciones">
+          <h3 class="titulos">SECCIONES</h3>
+          <section>
+          <div class="menuseccion">
+           <ul class="menuseccion">
+              <li><a href="">INICIO</a></li>
+              <li><a href="">PROYECTOS</a></li>
+              <li><a href="">SERVICIOS</a></li>
+              <li><a href="">NOSOTROS</a></li>
+              <li><a href="">CONTACTENOS</a></li>
+          </ul>
+        </div>
+          </section>
+        </div>
+            <div class="contactos">
+            <h3 class="titulos">CONTACTOS</h3>
+              <section class="contactos">
+  				      <ion-icon name="home"></ion-icon><p>DUARDO Arquitectos SL / P. de la Catellana 79, Madrid 28046.</p>
+                <ion-icon name="call"></ion-icon>
+                <p>
+                  <a href=123-456-7890>(+34) 947 002 963</a>
+               </p>
+                  <ion-icon name="mail"></ion-icon>
+               <p>
+                 <a href="mailto:asa.mendozaunidos@gmail.com">asa.mendozaunidos@gmail.com</a>
+               </p>
+             </section>
+           </div>
 
       <div class="redes">
-        <h3>REDES</h3>
-        <ion-icon name="logo-facebook"></ion-icon>
-        <ion-icon name="logo-googleplus"></ion-icon>
-        <ion-icon name="logo-twitter"></ion-icon>
-        <ion-icon name="logo-instagram"></ion-icon>
-        <ion-icon name="logo-skype"></ion-icon>
+        <h3 class="titulos">REDES</h3>
+        <div class="iconosredes">
+        <a href="https://twitter.com/"><i class="fab fa-twitter" ></i></a>
+        <a href="https://es-la.facebook.com/"><i class="fab fa-facebook"></i></a>
+        <a href="https://www.instagram.com/?hl=es-la"><i class="fab fa-instagram"></i></a>
+        <a href="https://www.google.com"><i class="fab fa-google-plus-g"></i></a>
+        <a href="https://www.skype.com">
+        <i class="fab fa-skype"></i></a>
+       </div>
       </div>
       <br>
       <div class="piedepagina">
-        <p>Diseñado y programado por
-        <b>bigtable.com - 2019°</b></p>
+        <p>Diseñado y programado por / bigtable.com - 2019</p>
+
       </div>
     </nav>
   </section>
+  </footer>
   </body>
 </html>
