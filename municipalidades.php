85aba7ac958804d26748676bfe769ce507957358
dmartinezbourio <50151600+dmartinezbourio@users.noreply.github.com>
2019-06-20 13:43:46 -0300

    Add files via upload
    Agrego archivo municipalidades.php

---
+++ municipalidades.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css" >
+    <link rel="shortcut icon" href="img/faviconasa.ico">
+    <link rel="stylesheet" href="css/muniForms.css">
+    <title>Municipalidades</title>
+  </head>
+  <body>
+    <div class="muni">
+        <img src="img/logo ASA.png" alt="logo">
+        <h1>Municipalidades</h1>
+
+
+          <div class="secciones">
+
+              <section class="seccionesmuni">
+                <h2>Capital</h2>
+              </section>
+
+              <section class="seccionesmuni">
+                <h2>Dalvian</h2>
+              </section>
+
+              <section class="seccionesmuni">
+                <h2>Godoy Cruz</h2>
+              </section>
+
+              <section class="seccionesmuni">
+                <h2>Guaymallen</h2>
+              </section>
+
+              <section class="seccionesmuni">
+                <h2>Junin</h2>
+              </section>
+
+              <section class="seccionesmuni">
+                <h2>Las Heras</h2>
+              </section>
+
+              <section class="seccionesmuni">
+                <h2>Lujan de Cuyo</h2>
+              </section>
+
+              <section class="seccionesmuni">
+                <h2>Maipu</h2>
+              </section>
+
+              <section class="seccionesmuni">
+                <h2>Malargue</h2>
+              </section>
+
+              <section class="seccionesmuni">
+                <h2>Rivadavia</h2>
+              </section>
+
+              <section class="seccionesmuni">
+                <h2>San Carlos</h2>
+              </section>
+
+              <section class="seccionesmuni">
+                <h2>San Martin</h2>
+              </section>
+
+              <section class="seccionesmuni">
+                <h2>San Rafael</h2>
+              </section>
+
+          </div>
+
+        </div>
+
+  </body>
+</html>
