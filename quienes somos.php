f03ba0be95fe2d27851b91f06ce1bf06b2a86df1
nicocastro84 <ncastro904@gmail.com>
2019-06-21 15:05:04 -0300

    creacion de quienes somos y css

---
+++ quienes somos.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>quienes somos</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link href="https://fonts.googleapis.com/css?family=Lato%7CMontserrat" rel="stylesheet">
+      <link rel="stylesheet" href="css/styleqsomos.css">
+  </head>
+  <body>
+    <div class="container">
+      <section class="titulo">
+        <h1 id="blanco">Asociación Solidaria</h1><h1 id="naranja"> de Arquitectos</h1>
+        <h3 id="blanco">Construimos la casa,</h3><h3 id="naranja"> tú la conviertes en un hogar</h3>
+      </section>
+      <section class="somos">
+        <p>La Asociación Solidaria de Arquitectos se está gestando como una Asociación Civil sin fines de lucro,
+           en donde los profesionales puedan compartir de forma genuina, la búsqueda de objetivos comunes en un espacio colaborativo y cooperativo.</p>
+      </section>
+      <section class="mision">
+      <h2 id="naranja">MISIÓN</h2>
+      <p>Promover la participación de los miembros, para resolver las distintas problemáticas del rubro y brindar capacitaciones para el desarrollo profesional</p>
+      </section>
+    <section class="vision">
+      <h2 id="naranja">VISIÓN</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </section>
+      <section class="enfoque">
+        <h2 id="blanco">NUESTRO</h2> <h2 id="naranja"> ENFOQUE</h2>
+        <p>Ser un espacio profesional, representativo de encuentro e interrelación que busca conciliar y lograr objetivos comunes</p>
+      </section>
+      <section class="comite">
+        <h2 id="naranja">COMITÉ</h2>
+        <article class="persona1">
+           <img class="main-photo" src="img/avatar/avatar1.jpg" alt="">
+           <p>nombre: lorem impsu</p>
+           <p>profesion: lorem impsu</p>
+           <p>contacto: lorem impsu</p>
+           <p>cargo: lorem impsu</p>
+         </article>
+         <article class="persona2">
+           <img class="main-photo" src="img/avatar/avatar2.jpg" alt="">
+           <p>nombre: lorem impsu</p>
+           <p>profesion: lorem impsu</p>
+           <p>contacto: lorem impsu</p>
+           <p>cargo: lorem impsu</p>
+         </article>
+         <article class="persona3">
+           <img class="main-photo" src="img/avatar/avatar3.jpg" alt="">
+           <p>nombre: lorem impsu</p>
+           <p>profesion: lorem impsu</p>
+           <p>contacto: lorem impsu</p>
+           <p>cargo: lorem impsu</p>
+         </article>
+      </section>
+    </div>
+
+
+
+  </body>
+</html>
