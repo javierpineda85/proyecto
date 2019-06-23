03efd39b95dd1a9eff863b17465cdb31691eea58
dmartinezbourio <50151600+dmartinezbourio@users.noreply.github.com>
2019-06-23 15:17:11 -0300

    Agregados botones en secciones

--- municipalidades.php
+++ municipalidades.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+  <!-- <meta http-equiv="Expires" content="0">
+  <meta http-equiv="Last-Modified" content="0">
+  <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+  <meta http-equiv="Pragma" content="no-cache"> -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
@@ -11,62 +15,64 @@
   </head>
   <body>
     <div class="muni">
-        <img src="img/logo ASA.png" alt="logo">
+        <div class="contenedordiv">
+        <img class="imgmuni" src="img/logo ASA.png" alt="logo">
+        </div>
         <h1>Municipalidades</h1>
 
 
           <div class="secciones">
 
               <section class="seccionesmuni">
-                <h2>Capital</h2>
+                <button class="boton" type="submit" name="capital"><h2>Capital</h2></button>
+              </section> 
+
+              <section class="seccionesmuni">
+                <button class="boton" type="submit" name="dalvian"><h2>Dalvian</h2></button>
               </section>
 
               <section class="seccionesmuni">
-                <h2>Dalvian</h2>
+                <button class="boton" type="submit" name="godoy_cruz"><h2>Godoy Cruz</h2></button>
               </section>
 
               <section class="seccionesmuni">
-                <h2>Godoy Cruz</h2>
+                <button class="boton" type="submit" name="guaymallen"><h2>Guaymallen</h2></button>
               </section>
 
               <section class="seccionesmuni">
-                <h2>Guaymallen</h2>
+                <button class="boton" type="submit" name="junin"><h2>Junin</h2></button>
               </section>
 
               <section class="seccionesmuni">
-                <h2>Junin</h2>
+                <button class="boton" type="submit" name="las_heras"><h2>Las Heras</h2></button>
               </section>
 
               <section class="seccionesmuni">
-                <h2>Las Heras</h2>
+                <button class="boton" type="submit" name="lujan"><h2>Lujan de Cuyo</h2></button>
               </section>
 
               <section class="seccionesmuni">
-                <h2>Lujan de Cuyo</h2>
+                <button class="boton" type="submit" name="maipu"><h2>Maipu</h2></button>
               </section>
 
               <section class="seccionesmuni">
-                <h2>Maipu</h2>
+                <button class="boton" type="submit" name="malargue"><h2>Malargue</h2></button>
               </section>
 
               <section class="seccionesmuni">
-                <h2>Malargue</h2>
+                <button class="boton" type="submit" name="rivadavia"><h2>Rivadavia</h2></button>
               </section>
 
               <section class="seccionesmuni">
-                <h2>Rivadavia</h2>
+                <button class="boton" type="submit" name="san_carlos"><h2>San Carlos</h2></button>
               </section>
 
               <section class="seccionesmuni">
-                <h2>San Carlos</h2>
+                <button class="boton" type="submit" name="san_martin"><h2>San Martin</h2></button>
               </section>
 
               <section class="seccionesmuni">
-                <h2>San Martin</h2>
-              </section>
-
-              <section class="seccionesmuni">
-                <h2>San Rafael</h2>
+                <button class="boton" type="submit" name="san_rafael"><h2>San Rafael</h2></button>
               </section>
 
           </div>
