00fd9230a552a0a43d82017a08521dc5e4271424
nicobende <niko_bendele@hotmail.com>
2019-06-30 22:51:18 -0300

    Servicios
    - Adjunto servicios.php y styleservicios.css.
    - adjunto imagen "serviciosimg.jpg
    - Ya lo relaciones con el footer y con el header
    - Faltaria hacer el hipervinculo en la solapa del header

---
+++ servicios.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="Pragma" content="no-cache">
+    <title>Servicios</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Personal CSS -->
+    <link rel="stylesheet" href="css/styleservicios.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Lato%7CMontserrat" rel="stylesheet">
+    <script src="https://unpkg.com/ionicons@4.5.5/dist/ionicons.js"></script>
+    <style type="text/css" media="screen">
+        A:link {
+            color: black;
+            text-decoration: none
+        }
+
+        A:hover {
+            color: black;
+            text-decoration: none
+        }
+
+        A:visited {
+            color: black;
+            text-decoration: none
+        }
+
+    </style>
+</head>
+
+<body>
+    <div class="menu">
+        <?php include_once ("header.php") ?>
+    </div>
+    <div class="textotitulo">
+        <h2 class="serv-texto1">NUESTROS SERVICIOS</h2>
+        <h2 class="serv-texto2">Abarcamos toda la gama de servicios, desde la planificación del proyecto a su construcción final. Contando también con el diseño de interiores y paisajes</h2>
+    </div>
+    <div class="containerserv">
+        <div class="servicios">
+            <h3>ARQUITECTURA</h3>
+            <h4>Del borrador a la construcción final.</h4>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+       </div>
+       <div class="interior">
+            <h3>DISEÑO INTERIOR</h3>
+            <h4>Trabajando en el espacio.</h4>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+    </div>
+    <script src="js/fontawesome-all.min.js"></script>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/up.js"></script>
+    <footer>
+        <?php include_once("footer.php") ?>
+    </footer>
+</body>
+
+</html>
