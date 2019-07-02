3b0f0733c3195113d311ad94a6d7291694ae53c9
Pablo Garcia Alvarez <pabloga90@gmail.com>
2019-07-02 00:06:01 -0300

    Ultima correccion a la seccion PreFrecuentes
    Ayudado por Nico, quedo bien responsive, le agrego algunos estilos para que quedara bonito. CSS limpio. Y se le agrego una background al titulo..

--- preguntasFrecuentes.php
+++ preguntasFrecuentes.php
@@ -8,9 +8,7 @@
 
     <!-- HOJAS DE STILOS-->
 
-   
     <link rel="stylesheet" href="css/preFrecuentes.css">
-   
 
     <!--FUENTES E ICONOS-->
 
@@ -21,41 +19,35 @@
     <title> Preguntas Frecuentes .. </title>
 </head>
 
-<body class="preFrecuentes">
-
-        <!----------------------------- Muestra el Header ----------------------------->
-        <div class="menu">
+<body>
+    <div class="menu">
         <?php include_once ("header.php") ?>
-        </div>
+    </div>
 
-    <div class="container" id="preFrecuentes">
+    <div class="container">
 
-            <div class="titulo">
-                <h3 id="blanco">PREGUNTAS</h3>
-
-                <h3 id="naranja">FRECUENTES</h3>
-            </div>
+        <h1>PREGUNTAS FRECUENTES</h1><br>
 
         <section class="preguntasFrecuentes">
             <h2>¿Pregunta Nº 1?</h2> <br>
             <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
         </section>
-        <br><br><br>
+        <br>
         <section class="preguntasFrecuentes">
             <h2>¿Pregunta Nº 2?</h2> <br>
             <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
         </section>
-        <br><br><br>
+        <br>
         <section class="preguntasFrecuentes">
             <h2>¿Pregunta Nº 3?</h2> <br>
             <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
         </section>
-        <br><br><br>
+        <br>
         <section class="preguntasFrecuentes">
             <h2>¿Pregunta Nº 4?</h2> <br>
             <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
         </section>
-        <br><br><br>
+        <br>
         <section class="preguntasFrecuentes">
             <h2>¿Pregunta Nº 5?</h2> <br>
             <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
