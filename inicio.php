3f33965055b1815a7b5c63f030000be83d56c2cf
Javier Pineda <javierpineda85@gmail.com>
2019-08-03 21:58:42 -0300

    Cambios en inicio

--- inicio.php
+++ inicio.php
@@ -16,9 +16,9 @@ $usuarioLogueado = traerUsuarioLogueado();
 
     <!-- la idea es comprobar si trae el usuario y lo muestro en el title-->
     <?php if ($usuarioLogueado != null) : ?>
-      <title>Bienvenido <?php echo $usuarioLogueado["name"]?></title>
+    <title>Bienvenido <?php echo $usuarioLogueado["name"]?></title>
     <?php else: ?>
-      <title>Bienvenido Invitado</title>
+    <title>Bienvenido Invitado</title>
     <?php endif; ?>
 
 
@@ -30,7 +30,7 @@ $usuarioLogueado = traerUsuarioLogueado();
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!--Personal CSS -->
     <link rel="stylesheet" href="css/style-header.css">
-     <!--Icono -->
+    <!--Icono -->
     <link rel="shortcut icon" href="img/faviconasa.ico">
     <!--FontAwesome -->
     <script src="https://kit.fontawesome.com/7907a05fb3.js"></script>
@@ -38,53 +38,55 @@ $usuarioLogueado = traerUsuarioLogueado();
 </head>
 
 <body>
-  <header>
-      <?php include_once ("header.php") ?>
-  </header>
-  <div class="slider">
-      <?php include_once ("slider.php") ?>
-  </div>
+    <header>
+        <?php include_once ("header.php") ?>
+    </header>
+    <div class="slider">
+        <?php include_once ("slider.php") ?>
+    </div>
 
-    
- <!--_____________________SECCIONES NAVEGABLES-->   
-<section class="capacitacion">
-    <h2>CAPACITACIONES</h2>
-    <spam>Porque nos interesa tu futuro, y queremos ayudarte a crecer. <br> Conocé los cursos que tenemos para ofrecerte</spam>
-</section>
 
-<section class="muni">
-    <h2>MUNICIPALIDADES</h2>
-    <spam>Accedé a la guía completa de trámites por Municipalidades. <br> Además encontrarás un organigrama para ayudarte a completasr los trámites</spam>
-</section>
-    
-<section class="proyecyo">
-    <h2>PROYECTOS</h2>
-    <spam>Estamos en constante movimiento. Conocé mas sobre nuestros proyectos. <br> También podés sumarte a nuestros proyectos</spam>
-</section>
-    
-<section class="nosotros">
-    <h2>NOSOTROS</h2>
-    <spam>Somos una organización sin fines de lucro. Conocémos un poco mas.</spam>
-</section>
-    
-<!--_____________________SECCIONES ESTATICAS-->
-<section class="d-flex align-content-center flex-wrap container-fluid w-100 text-white bg-black p-3 m-auto align-items-center">
-  <h2 class="m-auto"><span class="color-orange">2x1</span> en cursos!!!</h2>
-</section>
+    <!--_____________________SECCIONES NAVEGABLES-->
 
-<section class="d-flex container-fluid w-100 text-white bg-dark p-3 m-auto align-items-center">
-  <div class="m-auto col-12 col-md-6">
-    <img src="img/tarjetas.png" width="100%" alt="tarjetas">
-  </div>
-</section>
+    <div class="inicio">
+        <section class="capacitacion">
+            <h2>CAPACITACIONES</h2>
+            <spam>Porque nos interesa tu futuro, y queremos ayudarte a crecer. <br> Conocé los cursos que tenemos para ofrecerte</spam>
+        </section>
 
-<section class="col-12 col-md-12 text-white bg-dark">
+        <section class="muni">
+            <h2>MUNICIPALIDADES</h2>
+            <spam>Accedé a la guía completa de trámites por Municipalidades. <br> Además encontrarás un organigrama para ayudarte a completasr los trámites</spam>
+        </section>
 
-</section>
+        <section class="proyecto">
+            <h2>PROYECTOS</h2>
+            <spam>Estamos en constante movimiento. Conocé mas sobre nuestros proyectos. <br> También podés sumarte a nuestros proyectos</spam>
+        </section>
 
-<footer>
-    <?php include_once("footer.php") ?>
-</footer>
+        <section class="nosotros">
+            <h2>NOSOTROS</h2>
+            <spam>Somos una organización sin fines de lucro. Conocémos un poco mas.</spam>
+        </section>
+    </div>
+    <!--_____________________SECCIONES ESTATICAS-->
+    <section class="d-flex align-content-center flex-wrap container-fluid w-100 text-white bg-black p-3 m-auto align-items-center">
+        <h2 class="m-auto"><span class="color-orange">2x1</span> en cursos!!!</h2>
+    </section>
+
+    <section class="d-flex container-fluid w-100 text-white bg-dark p-3 m-auto align-items-center">
+        <div class="m-auto col-12 col-md-6">
+            <img src="img/tarjetas.png" width="100%" alt="tarjetas">
+        </div>
+    </section>
+
+    <section class="col-12 col-md-12 text-white bg-dark">
+
+    </section>
+
+    <footer>
+        <?php include_once("footer.php") ?>
+    </footer>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
