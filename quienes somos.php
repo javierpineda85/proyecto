0b0680383d4dc5cae8d01bcf2f686215e553baf8
Pablo Garcia Alvarez <pabloga90@gmail.com>
2019-08-06 21:28:28 -0300

    Prueba Restitucion 2
    Prueba 2

--- quienes somos.php
+++ quienes somos.php
@@ -29,10 +29,6 @@
         <?php include_once ("header.php") ?>
     </div>
 
-    <div class="containerqsomos">
-        <section class="titulo">
-            <h1> Asociación Solidaria de Arquitectos </h1>
-        </section>
 
     <div class="containerqsomos bg-image-white">
 
@@ -40,26 +36,26 @@
           <h2 class="banner-texto-standard"><span>ASOCIACION SOLIDARIA DE ARQUITECTOS</span></h2>
       </div>
         <section class="subtitulo">
-            <h3 class="naranja">Construimos la casa, tú la conviertes en un hogar.</h3>
+            <h3 id="blanco">Construimos la casa, tú la conviertes en un hogar.</h3>
         </section>
         <section class="somos">
           <br>
             <p>La Asociación Solidaria de Arquitectos se está gestando como una Asociación Civil sin fines de lucro, en donde los profesionales puedan compartir de forma genuina, la búsqueda de objetivos comunes en un espacio colaborativo y cooperativo.</p>
         </section>
         <section class="mision">
-            <h2 class="bg-dark"> MISIÓN </h2>
+            <h2 id="naranja">● MISIÓN ●</h2>
             <p>Promover la participación de los miembros, para resolver las distintas problemáticas del rubro y brindar capacitaciones para el desarrollo profesional</p>
         </section>
         <section class="vision">
-            <h2 class="naranja"> VISIÓN </h2>
+            <h2 id="naranja">● VISIÓN ●</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </section>
         <section class="enfoque">
-            <h2 class="bg-dark">NUESTRO ENFOQUE</h2>
+            <h2 id="naranja">●NUESTRO ENFOQUE●</h2>
             <p>Ser un espacio profesional, representativo de encuentro e interrelación que busca conciliar y lograr objetivos comunes</p>
         </section>
         <section class="comite">
-            <h2 class="naranja"> COMITÉ </h2>
+            <h2 id="naranja">● COMITÉ ●</h2>
             <article class="persona1">
                 <img class="main-photo" src="img/avatar/avatar1.jpg" alt="">
                 <p><b>Nombre:</b> lorem impsu</p>
